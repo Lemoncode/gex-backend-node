78201f966e2bd0ddf3674c67545c358944611347
Manolo <manudous@gmail.com>
2024-11-07 22:49:44 +0100

    wip

--- src/dals/user/user.repository.ts
+++ src/dals/user/user.repository.ts
@@ -3,7 +3,7 @@ import { User } from "./user.model.js";
 
 export const userRepository = {
   getUserList: async (page?: number, pageSize?: number): Promise<User[]> => {
-    const skip = Boolean(page) ? (page - 1) * pageSize : 0;
+              const skip = Boolean(page) ? (page - 1) * pageSize : 0;
     const limit = pageSize ?? 0;
 
     return await getUserContext().find().skip(skip).limit(limit).toArray();
