11d580f6085257e21f276560d30372fafa52e15f
Manuel Dorado Serrano <manueldoradoserrano@MacBook-Pro-de-Manuel.local>
2024-11-07 21:15:55 +0100

    remove cors

--- src/core/servers/rest-api.server.ts
+++ src/core/servers/rest-api.server.ts
@@ -1,16 +1,8 @@
 import express from "express";
-import cors from "cors";
-import { ENV } from "../constants/env.constants.js";
 
 export const createRestApiServer = () => {
   const app = express();
   app.use(express.json());
-  app.use(
-    cors({
-      methods: ENV.CORS_METHODS,
-      origin: ENV.CORS_ORIGIN,
-    })
-  );
 
   return app;
 };
