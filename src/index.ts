b2ca74e010a2d43e908f13470f2484d22a85030d
Manuel Dorado Serrano <manueldoradoserrano@MacBook-Pro-de-Manuel.local>
2024-11-07 13:49:22 +0100

    add winston for logs

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import express from "express";
 import path from "node:path";
+import {
+  logErrorRequestMiddleware,
+  logRequestMiddleware,
+} from "#common/middlewares/index.js";
+import { logger } from "#core/logger/index.js";
 import { createRestApiServer } from "#core/servers/index.js";
 import { ENV } from "#core/constants/index.js";
 import { userApi } from "#pods/user/index.js";
 
 const app = createRestApiServer();
 
+app.use(logRequestMiddleware(logger));
+
 app.use("/api/user", userApi);
+
+app.use(logErrorRequestMiddleware(logger));
+
+app.listen(ENV.PORT, () => {
+  logger.info(`Server ready at port ${ENV.PORT}`);
+});
 
 app.use(
   "/",
   express.static(path.resolve(import.meta.dirname, ENV.STATIC_FILES_PATH))
 );
-
-app.listen(ENV.PORT, () => {
-  console.log(`Server ready at port ${ENV.PORT}`);
-});
