670cbf995eb28c854e81f213122e21fb9fa12103
patrixampm <patrick.a.peckham@gmail.com>
2024-11-22 11:50:32 +0100

    endpoint para un solo usuario con id en la url hecho

--- src/pods/user/user.api.ts
+++ src/pods/user/user.api.ts
@@ -1,17 +1,31 @@
 import { Router } from 'express';
 import { userRepository } from '#dals/user/user.repository.js';
-import { mapUserListFromModelToApi } from './user.mappers.js';
+import { mapUserListFromModelToApi, mapUserFromModelToApi } from './user.mappers.js';
 
 export const userApi = Router();
 
-userApi.get('/', async (req, res, next) => {
-  try {
-    const page = Number(req.query.page);
-    const pageSize = Number(req.query.pageSize);
-    const userList = await userRepository.getUserList(page, pageSize);
+userApi
+  .get('/', async (req, res, next) => {
+    try {
+      const page = Number(req.query.page);
+      const pageSize = Number(req.query.pageSize);
+      const userList = await userRepository.getUserList(page, pageSize);
 
-    res.send(mapUserListFromModelToApi(userList));
-  } catch (error) {
-    next(error);
-  }
-});
+      res.send(mapUserListFromModelToApi(userList));
+    } catch (error) {
+      next(error);
+    }
+  })
+  .get('/:id', async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await userRepository.getUser(id);
+      if (user) {
+        res.send(mapUserFromModelToApi(user));
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      next(error);
+    }
+  });
