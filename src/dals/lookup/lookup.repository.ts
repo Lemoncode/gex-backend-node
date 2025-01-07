6be5d3a450151fc7b2be08afee9c06dde1da6c0a
Manolo <manudous@gmail.com>
2025-01-07 14:10:48 +0100

    create unidad-rol endpoint

--- src/dals/lookup/lookup.repository.ts
+++ src/dals/lookup/lookup.repository.ts
@@ -1,8 +1,17 @@
 import { Lookup } from '#common/models/index.js';
 import { getUnidadesProponentesContext } from './unidad-proponente.context.js';
 import { getRolesContext } from './role.context.js';
+import { UnidadRolList } from './lookup.model.js';
 
 export const lookupRepository = {
   getUnidadList: async (): Promise<Lookup[]> => await getUnidadesProponentesContext().find().toArray(),
   getRolList: async (): Promise<Lookup[]> => await getRolesContext().find().toArray(),
+  getUnidadRolList: async (): Promise<UnidadRolList> => {
+    const unidades = await getUnidadesProponentesContext().find().toArray();
+    const roles = await getRolesContext().find().toArray();
+    return {
+      roles,
+      unidades,
+    };
+  },
 };
