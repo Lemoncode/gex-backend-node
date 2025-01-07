b94bfbe2de8b5ded45eee0cac78c1c24685d6153
Manolo <manudous@gmail.com>
2025-01-07 11:25:58 +0100

    simplify getUnidadesList method and update API endpoint

--- src/dals/lookups/lookup.repository.ts
+++ src/dals/lookups/lookup.repository.ts
@@ -2,9 +2,5 @@ import { Lookup } from '#common/models/index.js';
 import { getUnidadesProponentesContext } from './lookup.context.js';
 
 export const lookupRepository = {
-  getUnidadesList: async (page?: number, pageSize?: number): Promise<Lookup[]> => {
-    const skip = Boolean(page) ? (page - 1) * pageSize : 0;
-    const limit = pageSize ?? 0;
-    return await getUnidadesProponentesContext().find().skip(skip).limit(limit).toArray();
-  },
+  getUnidadesList: async (): Promise<Lookup[]> => await getUnidadesProponentesContext().find().toArray(),
 };
--- src/pods/lookups/lookup.api.ts
+++ src/pods/lookups/lookup.api.ts
@@ -6,9 +6,7 @@ export const lookupApi = Router();
 
 lookupApi.get('/unidad', async (req, res, next) => {
   try {
-    const page = Number(req.query.page);
-    const pageSize = Number(req.query.pageSize);
-    const unidadesList = await lookupRepository.getUnidadesList(page, pageSize);
+    const unidadesList = await lookupRepository.getUnidadesList();
 
     res.send(mapLookupListFromModelToApi(unidadesList));
   } catch (error) {
